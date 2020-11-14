5abc9e35fd07d4dc0ff32ba84f242bbef6f77f05
Richard Muraguri <muraguri2005@gmail.com>
2020-11-14 10:40:52 +0300

    output management and development

--- src/index.js
+++ src/index.js
@@ -1,27 +1,16 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png'
-import Data from './data.xml'
-import Notes from './data.csv';
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
+import printMe from "./print";
 
-console.log(toml.title);
-console.log(toml.owner.name)
-console.log(yaml.title);
-console.log(yaml.owner.name)
-console.log(json.title);
-console.log(json.owner.name)
 function component() {
     const element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-    const myIcon = new Image();
-    myIcon.src = Icon;
-    element.appendChild(myIcon);
-    console.log(Data);
-    console.log(Notes)
+
+    const btn = document.createElement('button');
+    element.innerHTML = _.join(['Hello', 'webpack? '], ' ');
+
+    btn.innerHTML = 'Click me and check console';
+    btn.onclick = printMe;
+    element.appendChild(btn);
+
     return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,25 @@
 const path = require('path');
-const toml = require('toml');
-const yaml = require('yamljs');
-const json5 = require('json5');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, 'dist')
+    mode: 'development',
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
     },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test:/\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource'
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource'
-            },
-            {
-                test: /\.(csv|tsv)$/i,
-                use: ['csv-loader']
-            },
-            {
-                test: /\.xml$/i,
-                use: ['xml-loader']
-            },
-            {
-                test: /\.toml$/i,
-                type: 'json',
-                parser: {
-                    parse: toml.parse
-                }
-            },
-            {
-                test: /\.yaml$/i,
-                type: 'json',
-                parser: {
-                    parse: yaml.parse
-                }
-            },
-            {
-                test: /\.json5$/i,
-                type: 'json',
-                parser: {
-                    parse: json5.parse
-                }
-            }
-        ]
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new CleanWebpackPlugin({cleanStaleWebpackAssets: false}),
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        })
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
     }
 };
